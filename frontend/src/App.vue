557eb4f65a40f8472f1ad8ad03ffde40cbcb07a6
Vincent Marlino <71843843+Marlino69@users.noreply.github.com>
2024-10-25 13:25:47 +0700

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -46,6 +46,7 @@ const getUser = async () => {
 
 
 const changeEmail = async () => {
+  const userIdValue = userId.value.trim();
   const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(newEmail.value);
   if (!validEmail) {
     alert("Please enter a valid email address.");
