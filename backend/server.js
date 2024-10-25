0129a3349d64dac371ca992dba5f491f94b003a3
Vincent Marlino <71843843+Marlino69@users.noreply.github.com>
2024-10-25 13:48:00 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,9 +46,9 @@ app.get('/api/user/:id', (req, res) => {
 });
 
 app.post('/api/user/:id/change-email', (req, res) => {
-  const newEmail = req.body.email;
-  const query = `UPDATE users SET email = ? WHERE id = ?`; // Query diubah menjadi prepared statement
   res.send(req.body)
+  // const newEmail = req.body.email;
+  const query = `UPDATE users SET email = ? WHERE id = ?`; // Query diubah menjadi prepared statement
   connection.run(query, [newEmail, req.params.id], function (err) {
     if (err) throw err;
     if (this.changes === 0 ) res.status(404).send('User not found');
